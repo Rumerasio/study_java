0acc6001e59bca16c2ea43b62754d9bd68a97776
Joseph <joseph0417@naver.com>
2022-07-26 17:06:32 +0900

    java 배우기 시작

---
+++ src/com/java/study/ch01/Hello.java
@@ -0,0 +1,39 @@
+package com.java.study.ch01;
+
+public class Hello {
+	public static void main(String[]args) {
+		
+		boolean abc= true;
+		
+		int width = 100;
+		String name ="한찬구";
+		
+		String nickname = "로제";
+		String dateOfBirth = "890515";
+		int gender= 2;
+		String email = "rozemari22@gmail.com";
+		String id ="escaida15";
+		String password;
+		
+		String idNumber="88551524";
+		int commentNumber = 12;
+		int likeNumber = 3;
+		int reportNumber = 0;
+		
+		System.out.println("논리값 abc = "+abc);
+		System.out.println(width + " = 는 너비");
+		System.out.println(name);
+		System.out.println("회원님의 닉네임은 '"+ nickname + "' 입니다");
+		System.out.println(nickname+"님의 생년월일은 "+ dateOfBirth + " 입니다");
+		System.out.println(nickname+"님의 성별은 "+ gender + " 입니다");
+		System.out.println(email);
+		System.out.println(id);
+		System.out.println("password 는 아직 안받음");
+		System.out.println(idNumber);
+		System.out.println(commentNumber);
+		System.out.println(likeNumber);
+		System.out.println(reportNumber);
+		
+	}
+
+}
