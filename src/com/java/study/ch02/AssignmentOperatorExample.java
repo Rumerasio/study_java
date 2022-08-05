88b11f628a1543c3eaf78d6055db2e029ae8936e
Joseph <joseph0417@naver.com>
2022-08-05 17:30:29 +0900

    연산자 추가분

---
+++ src/com/java/study/ch02/AssignmentOperatorExample.java
@@ -0,0 +1,26 @@
+package com.java.study.ch02;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+		
+		// +=    -= 
+		
+		int result = 0;
+		
+		result +=5;
+		// ㄴ result = result +5;
+		
+		System.out.println("result = "+result);
+		
+		result -= 2;
+		
+		System.out.println("result = "+result);
+		
+		result *= 7;
+		System.out.println("result = "+result);
+		
+				
+	}
+
+}
