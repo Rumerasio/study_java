3e05f1801284c250b1153f4070f32717fc5bfe0d
Joseph <joseph0417@naver.com>
2022-08-08 17:32:37 +0900

    java if문

---
+++ src/com/java/study/ch03/IfExample2.java
@@ -0,0 +1,44 @@
+package com.java.study.ch03;
+
+public class IfExample2 {
+
+	public static void main(String[] args) {
+		
+		int vipClass= 5;
+		double price = 20000;
+		int sale;
+		double afterSale;
+		
+		System.out.println("정가: "+price);
+		if(vipClass == 1) {
+			sale = 5;
+			afterSale = price*(1-(0.01)*sale);
+			System.out.println("일반등급 "+sale+"%의 할인이 적용되어 "+afterSale+"입니다");
+		}
+		else if(vipClass ==2 ) {
+			sale = 10;
+			afterSale = price*(1-(0.01)*sale);
+			System.out.println("브론즈등급 "+sale+"%의 할인이 적용되어 "+afterSale+"입니다");
+		}
+		else if(vipClass ==3 ) {
+			sale = 15;
+			afterSale = price*(1-(0.01)*sale);
+			System.out.println("실버등급 "+sale+"%의 할인이 적용되어 "+afterSale+"입니다");
+		}
+		else if(vipClass ==4 ) {
+			sale = 20;
+			afterSale = price*(1-(0.01)*sale);
+			System.out.println("골드등급 "+sale+"%의 할인이 적용되어 "+afterSale+"입니다");
+		}
+		else if(vipClass ==5 ) {
+			sale = 27;
+			afterSale = price*(1-(0.01)*sale);
+			System.out.println("플래티넘등급 "+sale+"%의 할인이 적용되어 "+afterSale+"입니다");
+		}
+		else {
+			System.out.println("적합한 등급(1~5의 정수값)을 입력해주세요");
+		}
+		
+	}
+
+}
